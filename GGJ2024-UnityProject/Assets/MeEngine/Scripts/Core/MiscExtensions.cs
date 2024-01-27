81583e9cb7d43f5abb0db77f0010f563c28c361f
Ennacirruh <cjhelton03@gmail.com>
2024-01-26 19:59:38 -0700

    3d to 2d

--- GGJ2024-UnityProject/Assets/MeEngine/Scripts/Core/MiscExtensions.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-public static class MiscExtensions 
-{
-    public static Int32 WrapShift(this Int32 input, int shift){
-        uint uInput = (uint)input;
-        return (Int32)(uInput << shift | uInput >> (32 - shift));
-    }
-}
--- GGJ2024-UnityProject/Assets/MeEngine/Scripts/Events/EventListenerAttribute.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace MeEngine.Events
-{
-	// Custom attribute that allows the registration of a function to an event with a SubscribeAll call.
-	[AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
-	public class EventListenerAttribute : Attribute
-	{}
-}
