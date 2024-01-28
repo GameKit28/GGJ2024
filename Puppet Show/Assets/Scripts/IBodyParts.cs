6e16f9a655274c818dc3acb28df2f003e310a557
zinc1866 <galen.metzger@du.edu>
2024-01-27 20:00:28 -0700

    Update IBodyParts.cs

--- Puppet Show/Assets/Scripts/IBodyParts.cs
+++ Puppet Show/Assets/Scripts/IBodyParts.cs
@@ -3,13 +3,10 @@ public interface IBodyParts
 {
     public enum BodyPart
     {
-        chest,
-        rightShoulder,
-        rightForearm,
-        rightHand,
-        leftShoulder,
-        leftForearm,
-        leftHand
+        Head,
+        Shoulder, 
+        Forearm,
+        Hand
     }
 
 }
