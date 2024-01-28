a9b035123e244f4b370d53100381bcdec4ebf8da
Kit Burgess <kburgess@gamedesignkit.net>
2024-01-28 13:29:56 -0700

    Start game over screen from game state;

--- Puppet Show/Assets/Scripts/Runtime/GameState/GameOverState.cs
+++ Puppet Show/Assets/Scripts/Runtime/GameState/GameOverState.cs
@@ -8,7 +8,15 @@ public partial class GameStateFSM : MeFsm
 {
     public class GameOverState : MeFsmState<GameStateFSM>
     {
-        
+        string scene = "GameOver";
+
+        protected override void EnterState()
+        {
+            if(SceneManager.GetActiveScene().name != scene) 
+            {
+                SceneManager.LoadScene(scene);
+            }
+        }
     }
 }
 
