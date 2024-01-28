b0939eef6070e113599edd1e74bb6b5d55b08d43
Kit Burgess <kburgess@gamedesignkit.net>
2024-01-27 18:06:30 -0700

    More game states;

---
+++ Puppet Show/Assets/Scripts/Runtime/GameState/MonsterDefeatedState.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MeEngine.FsmManagement;
+using UnityEngine.SceneManagement;
+
+public partial class GameStateFSM : MeFsm 
+{
+    public class MonsterDefeatedState : MeFsmState<GameStateFSM>
+    {
+        
+    }
+}
+
